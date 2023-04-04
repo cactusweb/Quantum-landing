90860ad2177cd8b9c4e6924d83cd125fe020fe7c
gaspromov <gaspromov@mail.ru>
2023-04-04 18:53:57 +0300

    Убрал все об НФТ из сниппета

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -42,20 +42,20 @@ export default function App({ Component, pageProps }) {
         <meta name="twitter:site" content="@Quantum_NFT" />
         <meta
           property="og:title"
-          content="Quantum IO | NFT Automation Cloud Software"
+          content="Quantum IO | Automation Cloud Software"
         />
         <meta property="og:url" content="https://qntmru.io/" />
         <meta
           name="twitter:title"
-          content="Quantum IO | NFT Automation Cloud Software"
+          content="Quantum IO | Automation Cloud Software"
         />
         <meta
           name="twitter:description"
-          content="NFT Automation Hands-free Cloud Software with Drop & Restock Automation and Innovative solver system. Supporting NFT Platforms: VeVe | Solana | Ethereum"
+          content="Automation Hands-free Cloud Software with Drop & Restock Automation and Innovative solver system."
         />
         <meta
           name="description"
-          content="NFT Automation Hands-free Cloud Software with Drop & Restock Automation and Innovative solver system. Supporting NFT Platforms: VeVe | Solana | Ethereum"
+          content="NFT Automation Hands-free Cloud Software with Drop & Restock Automation and Innovative solver system."
         />
       </Head>
       <GlobalStyle />
